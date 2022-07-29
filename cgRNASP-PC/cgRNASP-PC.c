d64453d75958c0f841749201d6c71215f09ac9c1
Tan-group <82859325+Tan-group@users.noreply.github.com>
2022-07-29 12:25:14 +0800

    Update cgRNASP-PC.c

--- cgRNASP-PC/cgRNASP-PC.c
+++ cgRNASP-PC/cgRNASP-PC.c
@@ -268,7 +268,7 @@ int main(int argc, char *argv[])
   if(strcmp(type2[n1],type2[n1+1])!=0 || strcmp(num[n1],num[n1+1])!=0 || strcmp(chain[n1],chain[n1+1])!=0)
    length++;
  
- energy = 1.0*energy1 + 0.5*energy2 + 1.2*energy3 + 6.2*energy4/fun(length);
+ energy = 1.0*energy1 + 0.6*energy2 + 1.1*energy3 + 6.2*energy4/fun(length);
  
  fprintf(FP,"%s     %lf kBT\n", file_name[nnn], energy);
  }
